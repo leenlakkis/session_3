ea0423cd3da645e705cf837e875dfc6908f955f1
leenlakkis <leen.lakkis@gmail.com>
2024-03-19 12:36:34 +0100

    Initial commit

---
+++ session_3.py
@@ -0,0 +1,14 @@
+print("hello world again!")
+
+
+print(5/5)
+a = 5
+b = 5
+print (a, b, id(a), id(b))
+print(a is b)
+print(a == b)
+a.append(4)
+print(a, b, id(a),id(b))
+print(a is b)
+print(a == b)
+a = 5
